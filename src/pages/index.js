850072f3956ff9cc34cbc943e1073e7b784c8e1c
Ayabonga Qwabi <aya@simply.co.za>
2025-01-12 21:21:27 +0200

    fix home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import { Poppins } from 'next/font/google';
 import Layout from '@/components/layout';
 import RenderPageComponents from '@/components/content/generator';
+import { useConfig } from '@/context/ConfigContext';
+import { useEffect, useState } from 'react';
 
 function Home({ theme }) {
-  const page = theme?.pages?.find((page) => page.id === 'homepage');
+  const config = useConfig();
+
+  const page = config?.pages?.find((page) => page.id === 'homepage');
   return (
     <Layout>
       {page && <RenderPageComponents items={page?.components} theme={theme} />}
