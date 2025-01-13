d0b5bb4d6d67a23ed0a1b4dd7f789e13345d9280
Ayabonga Qwabi <aya@simply.co.za>
2025-01-13 17:15:46 +0200

    delete repo contents

--- src/components/ProgresssBarContainer/index.js
+++
@@ -1,6 +0,0 @@
-import { useConfig } from '@/context/ConfigContext';
-import NextNProgress from 'nextjs-progressbar';
-export default function App() {
-  const config = useConfig();
-  return <NextNProgress color={config?.progressColor} />;
-}
