8976e5aa4b375c779028573394ec7b5e4413beb4
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-05-05 17:16:53 +0200

    add seo content

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -13,6 +13,33 @@ const Layout = ({ children }) => {
         <>
           <Head>
             <title>{config?.partnerName}</title>
+            <meta
+              name='viewport'
+              content='width=device-width, initial-scale=1'
+            />
+            <meta name='description' content={config?.about} />
+            <meta
+              name='keywords'
+              content='Non profit organisation, donation platform, platform for impact, platform to donate, website donations'
+            />
+            <meta name='author' content={config?.partnerName} />
+            <meta name='og:image' content='https://inaethe.co.za/ogimage.png' />
+            <meta name='og:url' content='https://inaethe.co.za/' />
+            <meta name='og:title' content={config?.partnerName} />
+            <meta name='og:description' content={config?.about} />
+            <meta name='og:type' content='website' />
+            <meta name='twitter:card' content='summary_large_image' />
+            <meta name='twitter:site' content='@inaethe' />
+            <meta name='twitter:title' content={config?.partnerName} />
+            <meta name='twitter:description' content={config?.about} />
+            <meta
+              name='twitter:image'
+              content='https://inaethe.co.za/ogimage.png'
+            />
+            <meta name='twitter:creator' content='@inaethe' />
+            <meta name='twitter:site' content='@inaethe' />
+            <meta name='twitter:domain' content='inaethe.co.za' />
+            <link rel='canonical' href='https://inaethe.co.za/' />
             <link rel='icon' href={config?.favicon?.ico} sizes='any' />
             <link rel='icon' href={config?.favicon?.appleTouch} sizes='any' />
             <link rel='icon' href={config?.favicon?.size32} sizes='any' />
